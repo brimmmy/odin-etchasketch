4f2a5e54b552b00399ec5333bc6ff5de2099a199
brimmmy <alyse.brimhall@gmail.com>
2022-02-05 16:13:06 +0100

    Correct sizing adjustments and prompts

--- javascript.js
+++ javascript.js
@@ -1,18 +1,37 @@
 const gridContainer = document.querySelector('#grid-container');
 
-let userInput = 4;
-let squares = userInput * userInput;
+let userInput = prompt("Please enter a number between 1-100");
 
-for (var i = 0; i < squares; i++) {
-    const gridBox = document.createElement('div');
-    gridBox.classList.add('gridbox')
-    gridContainer.appendChild(gridBox);
-    let gridHover = document.getElementsByClassName('gridbox')[i];
 
-    gridHover.addEventListener("mouseover", function( event ) {
-    event.target.style.backgroundColor = "green";
-});
+function resetPrompt() {
+    window.location.reload();
+
+    if (userInput < 101 && userInput > 0) {
+        return;
+    } else {
+        alert ("please enter a valid number")
+        return resetPrompt();
+    };
 };
 
+let squares = userInput * userInput;
+function makeGrid (rows, cols) {
 
-// '#' + Math.random().toString(16).substr(-6); rando color generator, but doesnt keep the color. lol.
+        gridContainer.style.setProperty('--grid-rows', rows);
+        gridContainer.style.setProperty('--grid-cols', cols);
+
+    for (var i = 0; i < (rows * cols); i++) {
+
+        let gridBox = document.createElement('div');
+        gridBox.classList.add('gridbox');
+        gridContainer.appendChild(gridBox);
+        let gridHover = document.getElementsByClassName('gridbox')[i];
+
+        gridHover.addEventListener("mouseover", function( event ) {
+        event.target.style.backgroundColor = '#' + Math.random().toString(16).substr(-6);
+});
+};
+};
+
+makeGrid(userInput, userInput);
+
